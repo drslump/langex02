9511edf49f088742e39b95b9c44f9cb989066877
Orestes Sanchez <orestes@tid.es>
2011-10-24 16:29:57 +0200

    POST Snippet CGI improvement and cleaning

--- scripts/snippet.py
+++ scripts/snippet.py
@@ -16,31 +16,41 @@ import os
 import sys
 import uuid
 
-# Read the configuration file
 
+cgitb.enable()
+
+# Read the configuration file
 print "Content-Type: text/html"     # HTML is following
 print                               # blank line, end of headers
 
 configFile= open( '../conf/config.json', 'rb')
 configuration= json.load( configFile)
-print configuration
-print configuration[ "paths"]
+#print configuration
+dataPath= configuration[ "paths"]["data"]
+#print configuration[ "paths"]["data"]
 
 env = os.environ
-print "ENV=",env
+#print "ENV=",env
 #"<br/>".join( [ "%s=%s" % k,v for (k,v) in os.environ] )
-if "REMOTE_USER" not in env or "REQUEST_METHOD" not in env:
+if "REMOTE_USER" not in env:
     print "<H1>Error</H1>"
-    print "Forbidden."
+    print "Missing REMOTE_USER"
     sys.exit()
+
+user= env["REMOTE_USER"]
+
+# Make sure the directory is available
+
+dataPath= os.path.join( dataPath, os.path.join( user, "snippet"))
+if not os.path.isdir( dataPath):
+    # create the directory
+    os.makedirs( dataPath)
 
 #if not env[ "REQUEST_METHOD" ] == "POST":
 #    print "<H1>Error</H1>"
 #    print "only POST method allowed."
 #    sys.exit()
 dt= datetime.datetime.utcnow().strftime( "%Y%m%d%H%M-")
-
-cgitb.enable()
 
 form = cgi.FieldStorage()
 
@@ -49,17 +59,18 @@ if "code" not in form or "language" not in form:
     print "Please fill in the code and language fields."
     sys.exit()
 
-snippet={ "language": "", "code": ""}
+snippet={ }
+#snippet={ "language": "", "code": ""}
 snippet[ "language"]= form["language"].value
 snippet[ "code"]= form["code"].value
-print "<p>language:", snippet[ "language"]
-print "<p>code:", snippet[ "code"]
+#print "<p>language:", snippet[ "language"]
+#print "<p>code:", snippet[ "code"]
 
-
-print "<br/>json dump", json.dumps( snippet)
+#print "<br/>json dump", json.dumps( snippet)
 snippetUUID= str(uuid.uuid1())
-filename= "".join( (dt, snippetUUID, ".json"))
-print "filename=", filename
+filename= os.path.join( dataPath, "".join( (dt, snippetUUID, ".json")))
+#print "filename=", filename
 
 output= open( filename, "wb")
 output.write( json.dumps( snippet))
+output.close()
